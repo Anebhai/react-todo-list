085f5e16184fa2bd801ba711b7020d6b5d77918b
Anees Iqbal <ane.iqbal@gmail.com>
2020-07-04 21:20:14 +0530

    TodoItem component clearlist and delete methods

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,9 +2,20 @@ import React, { Component } from 'react';
 
 export default class Item extends Component {
   render() {
+    const { title, handleEdit, handleDelete } = this.props;
     return (
       <div>
-        <h3>Hello i am todo item</h3>
+        <li className="list-group-item d-flex my-2 justify-content-between">
+          <h6>{title}</h6>
+          <div className="todo-icon">
+            <span className="mx-2 text-success">
+              <i className="fas fa-pen" onClick={handleEdit}></i>
+            </span>
+            <span className="mx-2 text-danger">
+              <i className="fas fa-trash" onClick={handleDelete}></i>
+            </span>
+          </div>
+        </li>
       </div>
     );
   }
