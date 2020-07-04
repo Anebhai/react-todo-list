84f493cfcc83288c935a172695954a96a2699105
Anees Iqbal <ane.iqbal@gmail.com>
2020-07-04 15:53:56 +0530

    first commit

---
+++ src/components/TodoItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Item extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Hello i am todo item</h3>
+      </div>
+    );
+  }
+}
