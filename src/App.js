78db9dd36ef654529321712147e59f5e8776ab0f
Anees Iqbal <ane.iqbal@gmail.com>
2020-07-04 16:07:06 +0530

    showing vs code github setup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
+
+//showing vs code github setup
+
 function App() {
   return (
     <div>
